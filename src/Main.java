ae9562d736c12b1b97170a2f2dbd3b8c577b948d
Максимов Дмитрий <demasig@yandex.ru>
2023-04-24 08:21:36 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //входные данные
+        int priceTicket = 500;
+        int partForMile = 20;
+
+        //расчет
+        int countMile;
+        countMile = priceTicket / partForMile;
+
+        System.out.println(countMile + " миль");
+    }
+}
